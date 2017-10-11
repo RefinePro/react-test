83236abe44479d769e6fb78636076e59bc03ab2e
Patrik Toma <patotoma@gmail.com>
2017-10-11 18:39:37 +0200

    test 4

--- src/components/ImproveThis.js
+++ src/components/ImproveThis.js
@@ -4,22 +4,30 @@ import Image from "./Image";
 import { Menu, MenuItem, MenuDivider } from "@blueprintjs/core";
 
 class ImproveThis extends Component {
-	static propTypes = {};
+	static propTypes = {
+		first_name: PropTypes.string.isRequired,
+		last_name: PropTypes.string.isRequired,
+	};
+
+	static defaultProps = {
+		first_name: 'John',
+		last_name: 'Doe',
+	}
 
 	constructor(props) {
 		super(props);
 		this.state = {
-			fullname: props.first_name + ' '+ props.last_name
+			fullname: `${props.first_name} ${props.last_name}`,
 		};
 	}
-	
+
 	componentDidMount() {}
 
 	componentWillReceiveProps(nextProps) {}
 
 	render() {
 		return (
-			<span>{{ this.state.fullname }}</span>
+			<span>{this.state.fullname}</span>
 		);
 	}
 }
